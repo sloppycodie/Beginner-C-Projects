067035fd619f2a604ba4ab4d8a9f6bd3ede3dc70
Jibon Chandra Barman <sloppycodie27@gmail.com>
2026-07-03 12:11:30 +0600

    Implement password check with attempt limit
    Added a loop for password attempts and a counter to limit attempts.

--- Password_Checker.c
+++ Password_Checker.c
@@ -2,16 +2,26 @@
 #include <string.h>
 
 int main() {
+    int count=0;
     char inp[50];
     char password[] = "Ji60n&017";
     printf("Please, Enter Your Password: ");
     fgets(inp,sizeof(inp),stdin);
     inp[strcspn(inp,"\n")] = '\0';
-    if (strcmp(inp,password)==0)
+    while (1)
     {
-        printf("Access Granted!");
-    }else{
-        printf("Access Denied!");
+        count++;
+        if (count==4)
+        {
+            printf("Too Many Failed Attempts\nAccess Terminated.");
+            break;
+        }
+        if (strcmp(inp,password)==0)
+        {
+            printf("Access Granted!");
+        }else{
+            printf("Access Denied!");
+        }
     }
     return 0;
 }
